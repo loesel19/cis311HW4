91154fe0f3d0fcbd5ee2d7cba7903ab859e7c7ae
loesel19 <aloesel2@gmail.com>
2022-02-10 20:08:48 -0500

    worked with the dictionarys and nested them

--- cis311HW4/Form1.Designer.vb
+++ cis311HW4/Form1.Designer.vb
@@ -22,10 +22,252 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.lstAllDishes = New System.Windows.Forms.ListBox()
+        Me.lstItemsInSelected = New System.Windows.Forms.ListBox()
+        Me.lstRawInSelected = New System.Windows.Forms.ListBox()
+        Me.lstAllPrepped = New System.Windows.Forms.ListBox()
+        Me.lstAllRaw = New System.Windows.Forms.ListBox()
+        Me.btnAddNewDish = New System.Windows.Forms.Button()
+        Me.btnAddNewPrepped = New System.Windows.Forms.Button()
+        Me.btnAddNewRaw = New System.Windows.Forms.Button()
+        Me.btnRmvPreppedItem = New System.Windows.Forms.Button()
+        Me.btnPreppedToSelected = New System.Windows.Forms.Button()
+        Me.btnRawtoPrepped = New System.Windows.Forms.Button()
+        Me.btnRmvRaw = New System.Windows.Forms.Button()
+        Me.txtDish = New System.Windows.Forms.TextBox()
+        Me.txtPrepped = New System.Windows.Forms.TextBox()
+        Me.txtRaw = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(94, 15)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "List of all Dishes:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 151)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(172, 15)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Prepped Items in Selected Dish:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(12, 334)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(221, 15)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Raw Ingreients in Selected Prepped Item:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(429, 151)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(136, 15)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "List of all Prepped Items:"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(429, 334)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(144, 15)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "List of all Raw Ingredients:"
+        '
+        'lstAllDishes
+        '
+        Me.lstAllDishes.FormattingEnabled = True
+        Me.lstAllDishes.ItemHeight = 15
+        Me.lstAllDishes.Location = New System.Drawing.Point(12, 27)
+        Me.lstAllDishes.Name = "lstAllDishes"
+        Me.lstAllDishes.Size = New System.Drawing.Size(687, 64)
+        Me.lstAllDishes.TabIndex = 5
+        '
+        'lstItemsInSelected
+        '
+        Me.lstItemsInSelected.FormattingEnabled = True
+        Me.lstItemsInSelected.ItemHeight = 15
+        Me.lstItemsInSelected.Location = New System.Drawing.Point(12, 169)
+        Me.lstItemsInSelected.Name = "lstItemsInSelected"
+        Me.lstItemsInSelected.Size = New System.Drawing.Size(270, 94)
+        Me.lstItemsInSelected.TabIndex = 6
+        '
+        'lstRawInSelected
+        '
+        Me.lstRawInSelected.FormattingEnabled = True
+        Me.lstRawInSelected.ItemHeight = 15
+        Me.lstRawInSelected.Location = New System.Drawing.Point(12, 352)
+        Me.lstRawInSelected.Name = "lstRawInSelected"
+        Me.lstRawInSelected.Size = New System.Drawing.Size(270, 94)
+        Me.lstRawInSelected.TabIndex = 7
+        '
+        'lstAllPrepped
+        '
+        Me.lstAllPrepped.FormattingEnabled = True
+        Me.lstAllPrepped.ItemHeight = 15
+        Me.lstAllPrepped.Location = New System.Drawing.Point(429, 169)
+        Me.lstAllPrepped.Name = "lstAllPrepped"
+        Me.lstAllPrepped.Size = New System.Drawing.Size(270, 94)
+        Me.lstAllPrepped.TabIndex = 8
+        '
+        'lstAllRaw
+        '
+        Me.lstAllRaw.FormattingEnabled = True
+        Me.lstAllRaw.ItemHeight = 15
+        Me.lstAllRaw.Location = New System.Drawing.Point(429, 352)
+        Me.lstAllRaw.Name = "lstAllRaw"
+        Me.lstAllRaw.Size = New System.Drawing.Size(270, 94)
+        Me.lstAllRaw.TabIndex = 9
+        '
+        'btnAddNewDish
+        '
+        Me.btnAddNewDish.Location = New System.Drawing.Point(429, 97)
+        Me.btnAddNewDish.Name = "btnAddNewDish"
+        Me.btnAddNewDish.Size = New System.Drawing.Size(69, 40)
+        Me.btnAddNewDish.TabIndex = 10
+        Me.btnAddNewDish.Text = "Add New Dish"
+        Me.btnAddNewDish.UseVisualStyleBackColor = True
+        '
+        'btnAddNewPrepped
+        '
+        Me.btnAddNewPrepped.Location = New System.Drawing.Point(429, 269)
+        Me.btnAddNewPrepped.Name = "btnAddNewPrepped"
+        Me.btnAddNewPrepped.Size = New System.Drawing.Size(69, 53)
+        Me.btnAddNewPrepped.TabIndex = 11
+        Me.btnAddNewPrepped.Text = "Add New Prepped Item"
+        Me.btnAddNewPrepped.UseVisualStyleBackColor = True
+        '
+        'btnAddNewRaw
+        '
+        Me.btnAddNewRaw.Location = New System.Drawing.Point(429, 452)
+        Me.btnAddNewRaw.Name = "btnAddNewRaw"
+        Me.btnAddNewRaw.Size = New System.Drawing.Size(64, 40)
+        Me.btnAddNewRaw.TabIndex = 12
+        Me.btnAddNewRaw.Text = "Add New Raw Ingr"
+        Me.btnAddNewRaw.UseVisualStyleBackColor = True
+        '
+        'btnRmvPreppedItem
+        '
+        Me.btnRmvPreppedItem.Location = New System.Drawing.Point(318, 223)
+        Me.btnRmvPreppedItem.Name = "btnRmvPreppedItem"
+        Me.btnRmvPreppedItem.Size = New System.Drawing.Size(72, 40)
+        Me.btnRmvPreppedItem.TabIndex = 13
+        Me.btnRmvPreppedItem.Text = "->"
+        Me.btnRmvPreppedItem.UseVisualStyleBackColor = True
+        '
+        'btnPreppedToSelected
+        '
+        Me.btnPreppedToSelected.Location = New System.Drawing.Point(318, 169)
+        Me.btnPreppedToSelected.Name = "btnPreppedToSelected"
+        Me.btnPreppedToSelected.Size = New System.Drawing.Size(72, 40)
+        Me.btnPreppedToSelected.TabIndex = 14
+        Me.btnPreppedToSelected.Text = "<-"
+        Me.btnPreppedToSelected.UseVisualStyleBackColor = True
+        '
+        'btnRawtoPrepped
+        '
+        Me.btnRawtoPrepped.Location = New System.Drawing.Point(318, 352)
+        Me.btnRawtoPrepped.Name = "btnRawtoPrepped"
+        Me.btnRawtoPrepped.Size = New System.Drawing.Size(72, 40)
+        Me.btnRawtoPrepped.TabIndex = 15
+        Me.btnRawtoPrepped.Text = "<-"
+        Me.btnRawtoPrepped.UseVisualStyleBackColor = True
+        '
+        'btnRmvRaw
+        '
+        Me.btnRmvRaw.Location = New System.Drawing.Point(318, 406)
+        Me.btnRmvRaw.Name = "btnRmvRaw"
+        Me.btnRmvRaw.Size = New System.Drawing.Size(72, 40)
+        Me.btnRmvRaw.TabIndex = 16
+        Me.btnRmvRaw.Text = "->"
+        Me.btnRmvRaw.UseVisualStyleBackColor = True
+        '
+        'txtDish
+        '
+        Me.txtDish.Location = New System.Drawing.Point(504, 107)
+        Me.txtDish.Name = "txtDish"
+        Me.txtDish.Size = New System.Drawing.Size(195, 23)
+        Me.txtDish.TabIndex = 17
+        '
+        'txtPrepped
+        '
+        Me.txtPrepped.Location = New System.Drawing.Point(504, 285)
+        Me.txtPrepped.Name = "txtPrepped"
+        Me.txtPrepped.Size = New System.Drawing.Size(195, 23)
+        Me.txtPrepped.TabIndex = 18
+        '
+        'txtRaw
+        '
+        Me.txtRaw.Location = New System.Drawing.Point(499, 462)
+        Me.txtRaw.Name = "txtRaw"
+        Me.txtRaw.Size = New System.Drawing.Size(200, 23)
+        Me.txtRaw.TabIndex = 19
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(717, 520)
+        Me.Controls.Add(Me.txtRaw)
+        Me.Controls.Add(Me.txtPrepped)
+        Me.Controls.Add(Me.txtDish)
+        Me.Controls.Add(Me.btnRmvRaw)
+        Me.Controls.Add(Me.btnRawtoPrepped)
+        Me.Controls.Add(Me.btnPreppedToSelected)
+        Me.Controls.Add(Me.btnRmvPreppedItem)
+        Me.Controls.Add(Me.btnAddNewRaw)
+        Me.Controls.Add(Me.btnAddNewPrepped)
+        Me.Controls.Add(Me.btnAddNewDish)
+        Me.Controls.Add(Me.lstAllRaw)
+        Me.Controls.Add(Me.lstAllPrepped)
+        Me.Controls.Add(Me.lstRawInSelected)
+        Me.Controls.Add(Me.lstItemsInSelected)
+        Me.Controls.Add(Me.lstAllDishes)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents lstAllDishes As ListBox
+    Friend WithEvents lstItemsInSelected As ListBox
+    Friend WithEvents lstRawInSelected As ListBox
+    Friend WithEvents lstAllPrepped As ListBox
+    Friend WithEvents lstAllRaw As ListBox
+    Friend WithEvents btnAddNewDish As Button
+    Friend WithEvents btnAddNewPrepped As Button
+    Friend WithEvents btnAddNewRaw As Button
+    Friend WithEvents btnRmvPreppedItem As Button
+    Friend WithEvents btnPreppedToSelected As Button
+    Friend WithEvents btnRawtoPrepped As Button
+    Friend WithEvents btnRmvRaw As Button
+    Friend WithEvents txtDish As TextBox
+    Friend WithEvents txtPrepped As TextBox
+    Friend WithEvents txtRaw As TextBox
 End Class
--- cis311HW4/Form1.vb
+++ cis311HW4/Form1.vb
@@ -1,3 +1,184 @@
-﻿Public Class Form1
+﻿Imports System.Text.RegularExpressions
+Public Class Form1
 
+
+    'GLOBAL VARIABLES GLOBAL VARIABLES GLOBAL VARIABLES GLOBAL VARIABLES GLOBAL VARIABLES GLOBAL VARIABLES 
+    'GLOBAL VARIABLES GLOBAL VARIABLES GLOBAL VARIABLES GLOBAL VARIABLES GLOBAL VARIABLES GLOBAL VARIABLES 
+    Dim dicDishes = New SortedDictionary(Of String, SortedDictionary(Of String, SortedDictionary(Of String, Integer)))
+    Dim dicPreppedItems = New SortedDictionary(Of String, SortedDictionary(Of String, Integer))
+    Dim dicRawIng = New SortedDictionary(Of String, Integer)
+
+    Sub Form1_Load(Sender As Object, e As EventArgs) Handles Me.Load
+        'load dictionarys starting from bottom up
+        'load raw ingredients
+        dicRawIng.Add("Beef Patty", 0)
+        dicRawIng.Add("Bun", 0)
+        dicRawIng.Add("Ketchup", 0)
+        dicRawIng.Add("Onion", 0)
+        dicRawIng.Add("Potato", 0)
+        dicRawIng.Add("Chicken", 0)
+        dicRawIng.Add("Mayonaise", 0)
+        dicRawIng.Add("Grape", 0)
+        dicRawIng.Add("Plate", 0)
+        dicRawIng.Add("Glass", 0)
+        dicRawIng.Add("Water", 0)
+        dicRawIng.Add("Basket", 0)
+        dicRawIng.Add("Soft Drink Syrup", 0)
+        'display keys in all raw ingredient list box
+        lstAllRaw.DataSource = New BindingSource(dicRawIng, Nothing)
+        lstAllRaw.DisplayMember = "Key"
+
+        'now make some prepped items and add them to the dictionary of prepped items
+        Dim dicHamburger = New SortedDictionary(Of String, Integer)
+        dicHamburger.Add("Beef Patty", 0)
+        dicHamburger.Add("Bun", 0)
+        dicHamburger.Add("Ketchup", 0)
+        dicHamburger.Add("Onion", 0)
+        dicHamburger.Add("Plate", 0)
+        'add the item to the dictionary of prepped items
+        dicPreppedItems.Add("Hamburger", dicHamburger)
+
+        Dim dicFries = New SortedDictionary(Of String, Integer)
+        dicFries.Add("Potato", 0)
+        dicFries.Add("Basket", 0)
+        dicPreppedItems.Add("Fries", dicFries)
+
+        Dim dicSoftDrink = New SortedDictionary(Of String, Integer)
+        dicSoftDrink.Add("Glass", 0)
+        dicSoftDrink.Add("Water", 0)
+        dicSoftDrink.Add("Soft Drink Syrup", 0)
+        dicPreppedItems.Add("Soft Drink", dicSoftDrink)
+
+        Dim dicChickenSalad = New SortedDictionary(Of String, Integer)
+        dicChickenSalad.Add("Chicken", 0)
+        dicChickenSalad.Add("Mayonaise", 0)
+        dicChickenSalad.Add("Grape", 0)
+        dicChickenSalad.Add("Plate", 0)
+        dicPreppedItems.Add("Chicken Salad", dicChickenSalad)
+
+        'display the keys in the listbox of all prepped items
+        lstAllPrepped.DataSource = New BindingSource(dicPreppedItems, Nothing)
+        lstAllPrepped.DisplayMember = "Key"
+
+        'now we add the dishes
+        Dim dicHamburgerPlatter = New SortedDictionary(Of String, SortedDictionary(Of String, Integer))
+        dicHamburgerPlatter.Add("Hamburger", dicHamburger)
+        dicHamburgerPlatter.Add("Soft Drink", dicSoftDrink)
+        dicHamburgerPlatter.Add("Fries", dicFries)
+        dicDishes.Add("Hamburger Platter", dicHamburgerPlatter)
+
+        Dim dicChickenSaladPlatter = New SortedDictionary(Of String, SortedDictionary(Of String, Integer))
+        dicChickenSaladPlatter.Add("Chicken Salad", dicChickenSalad)
+        dicChickenSaladPlatter.Add("Soft Drink", dicSoftDrink)
+        dicChickenSaladPlatter.Add("Fries", dicFries)
+        dicDishes.Add("Chicken Salad Platter", dicChickenSaladPlatter)
+
+        'set listbox of all dishes to show the keys of dicDishes
+        lstAllDishes.DataSource = New BindingSource(dicDishes, Nothing)
+        lstAllDishes.DisplayMember = "Key"
+
+
+
+    End Sub
+
+    Sub Dish_Changed(sender As Object, e As EventArgs) Handles lstAllDishes.SelectedIndexChanged
+        'we need to first display the prepped items in selected dish
+        'we have to lookup the selected dish name in the dictionary of dishes
+        Dim strDishName = lstAllDishes.SelectedItem.Key.ToString
+        Dim dicSelectedItem = New SortedDictionary(Of String, SortedDictionary(Of String, Integer))
+        For Each entry As KeyValuePair(Of String, SortedDictionary(Of String, SortedDictionary(Of String, Integer))) In dicDishes
+            If entry.Key.ToString.Equals(strDishName) Then
+                dicSelectedItem = entry.Value
+            End If
+        Next
+        'check to see that there is a Key value pair in dicSelectedItem
+        If dicSelectedItem.Count > 0 Then
+            'clear listbox first
+            lstItemsInSelected.Items.Clear()
+            For Each entry As KeyValuePair(Of String, SortedDictionary(Of String, Integer)) In dicSelectedItem
+                lstItemsInSelected.Items.Add(entry.Key)
+            Next
+        Else
+            'here we instruct the user to add prepped items to the dish
+            MessageBox.Show("Please add Prepped items to the selected dish by hitting the <- button.")
+        End If
+
+    End Sub
+
+    Sub PreppedItem_Changed(sender As Object, e As EventArgs) Handles lstAllPrepped.SelectedIndexChanged
+        'get selected item as string, not we say selectedItem.Key since since the listbox displays the key, but
+        'the items attatched are still KeyValuePairs
+        Dim dicSelectedItem = New SortedDictionary(Of String, Integer)
+        Dim strItemName = lstAllPrepped.SelectedItem.Key.ToString
+
+        'loop through all keyValPairs in dicPreppedItems to see which Key matches our the key of the selected
+        'listbox item
+        For Each entry As KeyValuePair(Of String, SortedDictionary(Of String, Integer)) In dicPreppedItems
+            If entry.Key.ToString.Equals(strItemName) Then
+                dicSelectedItem = entry.Value
+            End If
+        Next
+        'check to make sure that we have raw ingredients in the selected prepped item
+        If dicSelectedItem.Count > 0 Then
+            'clear the listbox so we dont get a mess of raw ingredients
+            lstRawInSelected.Items.Clear()
+            'now we loop through the dictionary of the selected item and add the strings of the contained value
+            'keyValuePairs Keys and add each key to the listbox of raw ingredients in selected item
+            For Each entry As KeyValuePair(Of String, Integer) In dicSelectedItem
+                lstRawInSelected.Items.Add(entry.Key)
+            Next
+        Else
+            'tell user to add raw ingredients to prepped item
+            MessageBox.Show("Please add raw ingredients to the selected prepped item.")
+        End If
+    End Sub
+
+    Sub AddNewDish(sender As Object, e As EventArgs) Handles btnAddNewDish.Click
+        'check to see that the dish is not already in our dictionary
+        If Not dicDishes.ContainsKey(txtDish.Text) Then
+            'make sure input is valid
+            Dim blnValid As Boolean = validateInput(txtDish)
+            If blnValid Then
+                'create a dictionary to hold the selected item of from the listbox of all prepped items (lstAllPrepped)
+                'and then we have to search through the dictionary of all prepped items (dicPreppedItems)
+                'using the string of the key currently selected in the listbox to get an instance of
+                'the dictionary we want, since the listbox is only populated as key value pair of type (String, whatever)
+                '(in this instance whatever would be SortedDictionary Of String, Integer).
+                Dim dicNewDishItems = New SortedDictionary(Of String, SortedDictionary(Of String, Integer))
+                Dim strItemName = lstAllPrepped.SelectedItem.Key.ToString
+                For Each entry As KeyValuePair(Of String, SortedDictionary(Of String, Integer)) In dicPreppedItems
+                    If entry.Key.ToString.Equals(strItemName) Then
+                        dicNewDishItems.Add(strItemName, entry.Value)
+                    End If
+                Next
+                dicDishes.Add(txtDish.Text, dicNewDishItems)
+                'update the listbox
+                lstAllDishes.DataSource = New BindingSource(dicDishes, Nothing)
+                lstAllDishes.DisplayMember = "Key"
+            End If
+        Else
+            MessageBox.Show("Cannot add duplicate dish.")
+        End If
+    End Sub
+
+    Function validateInput(txtSender As TextBox) As Boolean
+        Dim blnValid As Boolean = True
+        'first check that the textbox contains only alphabetical characters and is not empty
+        'If a check is not met we set blnContinue to false
+        If txtSender.Text.Length = 0 Then
+            blnValid = False
+            'set focus on the sender
+            txtSender.Select()
+            MessageBox.Show("Text of new item can not be empty")
+        ElseIf Not Regex.IsMatch(txtSender.Text, "^[A-Za-z0-9']+$") Then
+            'clear textbox
+            txtSender.Text = ""
+            'set focus on the sender
+            txtSender.Select()
+            blnValid = False
+            MessageBox.Show("Text of new item can only contain letters, numbers and '")
+        End If
+
+        Return blnValid
+    End Function
 End Class
